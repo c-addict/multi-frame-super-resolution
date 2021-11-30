708d56ceea52930dede85833345ed2b61719c555
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2021-11-30 11:24:50 +0300

    Initial Commit

---
+++ multi-frame-super-resolution/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  multi-frame-super-resolution
+//
+//  Created by Николай Зеленев on 30.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ multi-frame-super-resolution/multi_frame_super_resolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  multi_frame_super_resolutionApp.swift
+//  multi-frame-super-resolution
+//
+//  Created by Николай Зеленев on 30.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct multi_frame_super_resolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
